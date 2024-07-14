423b3030801b3ee96c51a1078f219f5b35b4410d
Naika <105005760+naikka@users.noreply.github.com>
2024-07-14 05:25:13 +0000

    latest fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import '../CSS/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import pasonglogos from '../assets/pasonglogos.png';
 
 export default function Home() {
@@ -6,32 +7,35 @@ export default function Home() {
         <>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-sm-12 col-md-12 col-lg-12 p-5 text-center'>
-                        <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>BARANGAY PASONG</h1>
-                        <h2 style={{ fontSize: '3rem' }}>INFORMATION SYSTEM</h2>
+                    <div className='col-sm-12 col-md-12 col-lg-12 p-5 d-flex align-items-center'>
+                        <div className='p-5 d-flex justify-content-start'>
+                            <img src={pasonglogos} className='img-fluid' alt='Pasong Image' style={{ width: '100px', height: '100px' }} />
+                        </div>
+                        <div className='text-center flex-grow-1'>
+                            <h1 style={{ fontSize: '3rem', fontWeight: 'bold' }}>BARANGAY PASONG</h1>
+                            <h2 style={{ fontSize: '2rem' }}>INFORMATION SYSTEM</h2>
+                        </div>
+                        <div className='p-5 d-flex justify-content-start'>
+                            <img src={pasonglogos} className='img-fluid' alt='Pasong Image' style={{ width: '100px', height: '100px' }} />
+                        </div>
                     </div>
-                </div>
-                <div className='row mt-5'>
-                    <div className='col-sm-12 col-md-6 col-lg-6 p-5'>
-                        <img src={pasonglogos} className='img-fluid' alt='Pasong Image' />
-                    </div>
-                    <div className='col-sm-12 col-md-6 col-lg-6 p-5 d-flex align-items-center'>
-                        <form style={{ maxWidth: '400px', margin: 'auto' }}>
+
+
+
+                    <div className='col-12 col-md-6 p-5 d-flex align-items-center'>
+                        <form className='w-100' style={{ maxWidth: '400px', margin: 'auto' }}>
                             <input className='form-control mb-2' placeholder='Username' style={{ fontSize: '1.2rem' }} />
                             <input className='form-control mb-3' type='password' placeholder='Password' style={{ fontSize: '1.2rem' }} />
-
                             <div className='text-end' style={{marginTop:'-15px', marginBottom:'15px'}}>
                                 <button type="button" className="btn btn-link" onClick={() => window.location.href = '/#'}>Forgot password?</button>
                             </div>
-
-                            <div className="d-grid" style={{width:'20rem'}}>
+                            <div className="d-grid gap-2">
                                 <button className="btn btn-primary" type="button">Log in</button>
-                                <div className="mt-3">
-                                    <center><h5>or</h5></center>
+                                <div className="mt-3 text-center">
+                                    <h5>or</h5>
                                 </div>
                                 <button className="btn btn-light" type="button">Sign up</button>
-
-                                <button className="btn btn-primary mt-5" type="button" onClick={() => window.location.href = '/#'} style={{marginTop:'2rem', height:'5rem', fontWeight:'bold', fontSize:'20px'}}>Log in as resident</button>
+                                <button className="btn btn-primary mt-5" type="button" onClick={() => window.location.href = '/#'} style={{ height:'5rem', fontWeight:'bold', fontSize:'20px' }}>Log in as resident</button>
                             </div>
                         </form>
                     </div>
