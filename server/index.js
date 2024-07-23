f821a8ac6e11783ac26eae96fbf5a9ea076ea93c
Naika <105005760+naikka@users.noreply.github.com>
2024-07-23 15:58:54 +0000

    process of connecting db

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+const db = require('/models')
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server is running")
+    });
+});
