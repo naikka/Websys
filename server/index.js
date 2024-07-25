07d9d61c3c0b51b0f83d93dfac8c6720d59d9d6d
Naika <105005760+naikka@users.noreply.github.com>
2024-07-25 04:47:42 +0000

    change

--- server/index.js
+++ server/index.js
@@ -1,21 +1,6 @@
+const express = require('express');
+const app = express();
 
-const express = require("express")
-const cors = require("cors")
-const mysql = require("mysql")
-const path = require("path")
-const app = express()
-
-
-const port = 5000
-
-const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"naika352088",
-    database:"websysdb",
-})
-
-
-app.listen(port, () =>{
-    console.log("Server is running");
-})
+app.listen (3002, () => {
+    console.log("Serving running on port 3002");
+});
